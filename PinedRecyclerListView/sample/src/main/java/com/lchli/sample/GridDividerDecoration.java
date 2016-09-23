7cc13fc6fe98a03122ae7e5b2f65e4497a218f18
lchli <lchli888@sohu.com>
2016-09-23 10:38:51 +0800

    add sample project.

--- PinedRecyclerListView/sample/src/main/java/com/lchli/sample/GridDividerDecoration.java
+++ PinedRecyclerListView/sample/src/main/java/com/lchli/sample/GridDividerDecoration.java
@@ -1,4 +1,4 @@
-package com.lchli.pinedrecyclerlistview.sample;
+package com.lchli.sample;
 
 import android.content.Context;
 import android.content.res.TypedArray;
--- PinedRecyclerListView/sample/src/main/java/com/lchli/sample/PinnedRecyclerViewDemoActivity.java
+++ PinedRecyclerListView/sample/src/main/java/com/lchli/sample/PinnedRecyclerViewDemoActivity.java
@@ -1,59 +1,79 @@
-package com.lchli.pinedrecyclerlistview.sample;
+package com.lchli.sample;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.lchli.pinedrecyclerlistview.R;
 import com.lchli.pinedrecyclerlistview.library.ListSectionData;
-import com.lchli.pinedrecyclerlistview.library.pinnedListView.AbsAdapter;
-import com.lchli.pinedrecyclerlistview.library.pinnedListView.PinnedListAdapter;
-import com.lchli.pinedrecyclerlistview.library.pinnedListView.PinnedListView;
+import com.lchli.pinedrecyclerlistview.library.pinnedRecyclerView.PinnedRecyclerAdapter;
+import com.lchli.pinedrecyclerlistview.library.pinnedRecyclerView.PinnedRecyclerView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.lchli.pinedrecyclerlistview.sample.PinnedRecyclerViewDemoActivity.TestAdapter.VIEW_TYPE_SECTION_1;
-import static com.lchli.pinedrecyclerlistview.sample.PinnedRecyclerViewDemoActivity.TestAdapter.VIEW_TYPE_SECTION_2;
-
 /**
  * @author lchli
- *         this sample show how to use different section in PinnedListView.
+ *         this sample show how to use different section in PinnedRecyclerView.
  */
-public class PinnedListViewDemoActivity extends AppCompatActivity {
+public class PinnedRecyclerViewDemoActivity extends AppCompatActivity {
 
-    private PinnedListView listView;
+    private PinnedRecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pinned_list_view);
+        setContentView(R.layout.activity_pinned_recycler_view);
 
-        listView = (PinnedListView) findViewById(R.id.listView);
+        recyclerView = (PinnedRecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        recyclerView.addItemDecoration(new GridDividerDecoration(this, GridDividerDecoration.VERTICAL_LIST));
 
         List all = new ArrayList();
         for (int i = 0; i < 100; i++) {
             all.add(new Person(i + ""));
             if (i == 10 || i == 20 || i == 90) {
-                TestSectionData sectionData = new TestSectionData(VIEW_TYPE_SECTION_1);
+                TestSectionData sectionData = new TestSectionData(TestAdapter.VIEW_TYPE_SECTION_1);
                 sectionData.extraData = i + "s";
                 all.add(sectionData);
             } else if (i == 3 || i == 23 || i == 63) {
-                TestSectionData sectionData = new TestSectionData(VIEW_TYPE_SECTION_2);
+                TestSectionData sectionData = new TestSectionData(TestAdapter.VIEW_TYPE_SECTION_2);
                 sectionData.extraData = i + "s";
                 all.add(sectionData);
             }
         }
 
         TestAdapter testAdapter = new TestAdapter();
-        listView.setPinnedAdapter(testAdapter);
+        recyclerView.setPinnedAdapter(testAdapter);
         testAdapter.refresh(all);
 
     }
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.recycler_demo_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.openListView:
+                Intent it = new Intent(this, PinnedListViewDemoActivity.class);
+                startActivity(it);
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     private static class TestSectionData extends ListSectionData {
 
@@ -73,17 +93,13 @@ public class PinnedListViewDemoActivity extends AppCompatActivity {
     }
 
 
-    class TestAdapter extends PinnedListAdapter {
+    class TestAdapter extends PinnedRecyclerAdapter {
 
         static final int VIEW_TYPE_SECTION_1 = 0;
         static final int VIEW_TYPE_SECTION_2 = 1;
 
         static final int VIEW_TYPE_ITEM = 2;
 
-        @Override
-        public int getViewTypeCount() {
-            return 3;
-        }
 
         @Override
         public int getItemViewType(int position) {
@@ -97,29 +113,29 @@ public class PinnedListViewDemoActivity extends AppCompatActivity {
         }
 
         @Override
-        public AbsAdapter.AbsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             View view;
             switch (viewType) {
                 case VIEW_TYPE_ITEM:
                     view = LayoutInflater.from(getApplicationContext())
                             .inflate(R.layout.list_item, parent, false);
-                    return new ListItemViewHolder(view, viewType);
+                    return new ListItemViewHolder(view);
 
                 case VIEW_TYPE_SECTION_1:
                     view = LayoutInflater.from(getApplicationContext())
                             .inflate(R.layout.list_item_section, parent, false);
-                    return new SectionViewHolder(view, viewType);
+                    return new SectionViewHolder(view);
 
                 case VIEW_TYPE_SECTION_2:
                     view = LayoutInflater.from(getApplicationContext())
                             .inflate(R.layout.list_item_section2, parent, false);
-                    return new Section2ViewHolder(view, viewType);
+                    return new Section2ViewHolder(view);
             }
             return null;
         }
 
         @Override
-        public void onBindViewHolder(AbsAdapter.AbsViewHolder holder, int position) {
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
             Object data = getItem(position);
             int viewType = getItemViewType(position);
 
@@ -144,54 +160,33 @@ public class PinnedListViewDemoActivity extends AppCompatActivity {
             }
         }
 
-        class ListItemViewHolder extends AbsAdapter.AbsViewHolder {
+        class ListItemViewHolder extends RecyclerView.ViewHolder {
 
             private final TextView textView;
-            private final View itemView;
 
-            public ListItemViewHolder(View itemView, int viewType) {
-                super(viewType);
-                this.itemView = itemView;
+            public ListItemViewHolder(View itemView) {
+                super(itemView);
                 textView = (TextView) itemView.findViewById(R.id.textView);
-            }
-
-            @Override
-            protected View getItemView() {
-                return itemView;
             }
         }
 
-        class SectionViewHolder extends AbsAdapter.AbsViewHolder {
+        class SectionViewHolder extends RecyclerView.ViewHolder {
 
             private final TextView textView;
-            private final View itemView;
 
-            public SectionViewHolder(View itemView, int viewType) {
-                super(viewType);
-                this.itemView = itemView;
+            public SectionViewHolder(View itemView) {
+                super(itemView);
                 textView = (TextView) itemView.findViewById(R.id.textView);
-            }
-
-            @Override
-            protected View getItemView() {
-                return itemView;
             }
         }
 
-        class Section2ViewHolder extends AbsAdapter.AbsViewHolder {
+        class Section2ViewHolder extends RecyclerView.ViewHolder {
 
             private final TextView textView;
-            private final View itemView;
 
-            public Section2ViewHolder(View itemView, int viewType) {
-                super(viewType);
-                this.itemView = itemView;
+            public Section2ViewHolder(View itemView) {
+                super(itemView);
                 textView = (TextView) itemView.findViewById(R.id.textView);
-            }
-
-            @Override
-            protected View getItemView() {
-                return itemView;
             }
         }
     }
